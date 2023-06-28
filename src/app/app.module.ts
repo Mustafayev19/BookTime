fd290dcc53625fa225737ac516f3e727f8326984
Mustafayev19 <azemustafayev19@gmail.com>
2023-06-28 19:09:53 +0400

    initial commit and Head-line component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { HeadLineComponent } from './head-line/head-line.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    MainComponent,
+    BookDetailComponent,
+    HeadLineComponent
   ],
   imports: [
     BrowserModule,
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'color1': '#F5F5F5',
+        'color2': '#F2EAD3',
+        'color3': '#DFD7BF',
+        'color4': '#3F2305',
+      },
+    },
+  },
+  plugins: [],
+}
